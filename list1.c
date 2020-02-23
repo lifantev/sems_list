abe0dd89d9682163bb3a6c71e1483f6547fd466f
LifantevDA <Lifantev.DA@phystech.edu>
2020-02-23 19:39:38 +0300

    print, push, pop funcs added

--- list1.c
+++ list1.c
@@ -10,8 +10,47 @@ typedef int elem_t;
 typedef struct Node
 {
     elem_t data;
-    struct Node* node;
+    struct Node* next;
 } Node;
+
+void print(Node** plist)
+{
+    for(Node *p = plist; p != NULL; p = p->next)
+        printf("%d ", p->data);
+    puts("");
+}
+
+void push(Node** plist, elem_t elem)
+{
+    if(is_empty(*plist))
+        puts("Empty list");
+    
+    Node* new_node = calloc(1, sizeof(Node));
+    new_node->next = *plist;
+    new_node->data = elem;
+    *plist = new_node; 
+}
+
+elem_t pop(Node** plist)
+{
+    if(*plist == NULL)
+    {
+        puts("Empty list");
+        return 0;
+    }
+
+    Node *p = *plist;
+    elem_t res = p->data;
+    *plist = p->next;
+    free(p);
+
+    return res;
+}
+
+int is_empty(Node* list)
+{
+    return list == NULL;
+}
 
 int main() 
 {

76834aa365b0e2c90fc23bb7a02f45a0fed0ed6e
LifantevDA <Lifantev.DA@phystech.edu>
2020-02-23 16:39:54 +0300

    init list

---
+++ list1.c
@@ -0,0 +1,19 @@
+// list1.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  
+#include <stdbool.h>
+
+typedef int elem_t;
+
+typedef struct Node
+{
+    elem_t data;
+    struct Node* node;
+} Node;
+
+int main() 
+{
+    return 0;
+}

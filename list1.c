28dc200a0df8ad4db7bac1a8ffbcaca1c10328b5
LifantevDA <Lifantev.DA@phystech.edu>
2020-02-24 13:58:06 +0300

    did some fixes to funcs

--- list1.c
+++ list1.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  
+#include <string.h>
 #include <stdbool.h>
 
 typedef int elem_t;
@@ -10,30 +10,38 @@ typedef int elem_t;
 typedef struct Node
 {
     elem_t data;
-    struct Node* next;
+    struct Node *next;
 } Node;
 
-void print(Node** plist)
+int is_empty(Node *list)
 {
-    for(Node *p = plist; p != NULL; p = p->next)
+    return list == NULL;
+}
+
+void print(Node *list)
+{
+    if (is_empty(list))
+    {
+        puts("Empty list");
+        return;
+    }
+
+    for (Node *p = list; p != NULL; p = p->next)
         printf("%d ", p->data);
     puts("");
 }
 
-void push(Node** plist, elem_t elem)
+void push(Node **plist, elem_t elem)
 {
-    if(is_empty(*plist))
-        puts("Empty list");
-    
-    Node* new_node = calloc(1, sizeof(Node));
+    Node *new_node = calloc(1, sizeof(Node));
     new_node->next = *plist;
     new_node->data = elem;
-    *plist = new_node; 
+    *plist = new_node;
 }
 
-elem_t pop(Node** plist)
+elem_t pop(Node **plist)
 {
-    if(*plist == NULL)
+    if (*plist == NULL)
     {
         puts("Empty list");
         return 0;
@@ -47,14 +55,74 @@ elem_t pop(Node** plist)
     return res;
 }
 
-int is_empty(Node* list)
+elem_t get(Node *list)
 {
-    return list == NULL;
+    return list->data;
 }
 
-int main() 
+int size(Node *list)
 {
-    Node** list = NULL;
-    
+    int size = 0;
+
+    while (list != NULL)
+    {
+        size++;
+        list = list->next;
+    }
+
+    return size;
+}
+
+void clear(Node **plist)
+{
+    Node *p = *plist;
+
+    while (*plist != NULL)
+    {
+        Node *p = *plist;
+        *plist = p->next;
+        free(p);
+    }
+}
+
+#ifdef MAIN_LIST1
+int main()
+{
+    Node *list = NULL;
+    push(&list, 1);
+    push(&list, 12);
+    push(&list, 13);
+    push(&list, 14);
+    push(&list, 15);
+    push(&list, 16);
+    print(list);
+    printf("%d", size(list));
+    int x = pop(&list);
+    print(list);
+    x = pop(&list);
+    x = pop(&list);
+    x = pop(&list);
+    x = pop(&list);
+    x = pop(&list);
+    print(list);
+
+    x = pop(&list);
+    print(list);
+    push(&list, 21);
+    push(&list, 21);
+
+    print(list);
+
+    clear(&list);
+
+    x = pop(&list);
+
+    push(&list, 31);
+
+    print(list);
+
+    clear(&list);
+
     return 0;
 }
+#endif // MAIN_LIST1
